3e8a71ea41bfffcb3f90af332e1914ac9ded769f
yeonnex <yeonnex@gmail.com>
2021-08-11 12:06:35 +0900

    comment 블럭에 delete 버튼 생성

---
+++ accountapp/views.py
@@ -0,0 +1,77 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
+from django.shortcuts import render
+
+# Create your views here.
+from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+
+from accountapp.decorators import account_ownership_required
+from accountapp.forms import AccountCreationForm
+from accountapp.models import HelloWorld
+
+
+@login_required
+def hello_world(request):
+    if request.method == "POST":
+
+        temp = request.POST.get('input')
+
+        new_data = HelloWorld()
+        new_data.text = temp
+        new_data.save()
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+
+    else:
+        data_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html',
+                      context={'data_list': data_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = 'accountapp/create.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
+
+
+has_ownership = [login_required, account_ownership_required]
+
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountCreationForm
+    context_object_name = 'target_user'
+    template_name = 'accountapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
+
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
+class AccountDeleteView(DeleteView):
+    model = User
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/delete.html'
+
+
+
+
+
+
---
+++ commentapp/views.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+# Create your views here.
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, DeleteView
+
+from articleapp.models import Article
+from commentapp.decorators import comment_ownership_required
+from commentapp.forms import CommentCreationForm
+from commentapp.models import Comment
+
+
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
+class CommentCreateView(CreateView):
+    model = Comment
+    form_class = CommentCreationForm
+    template_name = 'commentapp/create.html'
+
+    def form_valid(self, form):
+        form.instance.writer = self.request.user
+        form.instance.article_id = self.request.POST.get('article_pk')
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
+
+
+@method_decorator(comment_ownership_required, 'get')
+@method_decorator(comment_ownership_required, 'post')
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
+
+
+
+
