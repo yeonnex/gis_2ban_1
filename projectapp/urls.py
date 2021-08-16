d2e5d46bea2cf3572473c96309dad7b6bac035fa
yeonnex <yeonnex@gmail.com>
2021-08-17 06:56:02 +0900

    projectapp: 게시판글 create(title, image, description) 하고 db에 저장

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,3 +1,10 @@
+from django.urls import path
+
+from projectapp.views import ProjectCreateView
+
+app_name = 'projectapp'
+
 urlpatterns = [
+    path('create/', ProjectCreateView.as_view(), name='create'),
 
 ]
