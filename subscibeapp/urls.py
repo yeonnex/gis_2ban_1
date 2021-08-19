454d4915f36a785383a789e49c8872a5950be1c9
yeonnex <yeonnex@gmail.com>
2021-08-19 18:24:35 +0900

    커밋

--- subscibeapp/urls.py
+++ subscibeapp/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = [
+from django.urls import path
 
-]
+from subscibeapp.views import SubscriptionView
+
+app_name = 'subscribeapp'
+
+urlpatterns = {
+    path('subscribe/<int:project_pk>', SubscriptionView.as_view(), name='subscribe')
+}
