e4bfb355116c7cb82a024b5a828d2e8100aa5ec9
yeonnex <yeonnex@gmail.com>
2021-08-25 11:19:45 +0900

    좋아요버튼 생성&누르면 +1 올라감

--- likeapp/urls.py
+++ likeapp/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = [
+from django.urls import path
 
+from likeapp.views import LikeArticleView
+
+app_name = 'likeapp'
+
+urlpatterns = [
+    path('articles/<int:article_pk>', LikeArticleView.as_view(), name='article_like')
 ]
