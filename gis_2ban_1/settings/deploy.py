2aa27a29150cf39ec03e930db2fbc7d90b992f25
yeonnex <yeonnex@gmail.com>
2021-09-14 18:40:03 +0900

    push

--- gis_2ban_1/settings/deploy.py
+++ gis_2ban_1/settings/deploy.py
@@ -5,7 +5,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.urls import reverse_lazy
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+
+# 이 부분이 base.py 파일이랑 중복으로 들어가있네요
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
 env_list = dict()
 
 local_emv = open(os.path.join(BASE_DIR, '.env'))
